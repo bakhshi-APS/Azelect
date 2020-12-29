2359b4db51bd22ac2d8f8e2f3bc1f97973a6eadb
= <niamatullahbakhshi@gmail.com>
2020-12-29 23:11:01 +0430

    feat: link angular, add fomantic

---
+++ src/main/kotlin/online/shopping/azelect/service/BooksService.kt
@@ -0,0 +1,22 @@
+package online.shopping.azelect.service
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/books")
+class BooksSece {
+
+    @GetMapping("/books")
+    @ResponseBody
+    fun getBooks(): BooksService {
+        return BooksService("Books Are retrieved... Success")
+    }
+}
+
+
+data class BooksService(
+        val status: String
+)
