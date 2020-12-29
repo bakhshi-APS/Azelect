e28f06e8d414272a08f1ab4bff310f8406c7732c
= <niamatullahbakhshi@gmail.com>
2020-12-29 21:33:39 +0430

    initial commit along with angular setup

---
+++ src/main/kotlin/online/shopping/azelect/AzelectApplication.kt
@@ -0,0 +1,11 @@
+package online.shopping.azelect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AzelectApplication
+
+fun main(args: Array<String>) {
+    runApplication<AzelectApplication>(*args)
+}
---
+++ src/test/kotlin/online/shopping/azelect/AzelectApplicationTests.kt
@@ -0,0 +1,13 @@
+package online.shopping.azelect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AzelectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
